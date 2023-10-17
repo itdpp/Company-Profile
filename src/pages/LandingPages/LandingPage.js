aac4dbde8b2fa10ee357a57029e555a0f0881547
saifulloh69 <saifullohyr69@gmail.com>
2023-10-17 10:49:55 +0700

    #

--- src/pages/LandingPages/LandingPage.js
+++ src/pages/LandingPages/LandingPage.js
@@ -50,7 +50,7 @@ const LandingPage = () => {
         <img
           src="https://www.uniquip.co.id/wp-content/uploads/2023/01/HYUNDAI-1-768x512.jpg"
           alt="product"
-          className="w-full h-full aspect-video bg-cover border border-gray-300 hover:-translate-y-3 hover:duration-300"
+          className="w-full h-full aspect-video bg-cover border border-gray-300 hover:scale-105 transition-transform duration-300 transform "
         />
         <div className="">
           <div className="text-center text-lg uppercase italic font-light sm:text-lg sm:line-clamp-2 lg:line-clamp-2 lg:text-lg xl:sm:line-clamp-none">
@@ -65,7 +65,7 @@ const LandingPage = () => {
         <img
           src="https://www.uniquip.co.id/wp-content/uploads/2023/01/HYUNDAI-1-768x512.jpg"
           alt="product"
-          className="w-full h-full aspect-video bg-cover border border-gray-300 hover:-translate-y-3 hover:duration-300"
+          className="w-full h-full aspect-video bg-cover border border-gray-300 hover:scale-105 transition-transform duration-300 transform "
         />
         <div className="">
           <div className="text-center text-lg uppercase italic font-light sm:text-lg sm:line-clamp-2 lg:line-clamp-2 lg:text-lg xl:sm:line-clamp-none">
@@ -80,7 +80,7 @@ const LandingPage = () => {
         <img
           src="https://www.uniquip.co.id/wp-content/uploads/2023/01/HYUNDAI-1-768x512.jpg"
           alt="product"
-          className="w-full h-full aspect-video bg-cover border border-gray-300 hover:-translate-y-3 hover:duration-300"
+          className="w-full h-full aspect-video bg-cover border border-gray-300 hover:scale-105 transition-transform duration-300 transform "
         />
         <div className="">
           <div className="text-center text-lg uppercase italic font-light sm:text-lg sm:line-clamp-2 lg:line-clamp-2 lg:text-lg xl:sm:line-clamp-none">
@@ -88,6 +88,22 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
+      <div
+        // onClick={() => navigate("/typescania")}
+        className="px-4 text-black text-left mb-4 sm:mb-12 md:w-64 lg:w-80 xl:w-96  "
+      >
+        <img
+          src="https://www.uniquip.co.id/wp-content/uploads/2023/01/HYUNDAI-1-768x512.jpg"
+          alt="product"
+          className="w-full h-full aspect-video bg-cover border border-gray-300 hover:scale-105 transition-transform duration-300 transform "
+        />
+        <div className="">
+          <div className="text-center text-lg uppercase italic font-light sm:text-lg sm:line-clamp-2 lg:line-clamp-2 lg:text-lg xl:sm:line-clamp-none">
+            Dump Truck
+          </div>
+        </div>
+      </div>
+      
     </div>
     <div className='mx-10 my-16'>
         <img className='mx-auto' src='https://www.uniquip.co.id/wp-content/uploads/2023/08/Logo-Principles-1-1024x139.jpg' alt=''/>
